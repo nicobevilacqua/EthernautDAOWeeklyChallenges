c4e90a30d3f384d693dfbfdfaf47de138523aed5
Nicolás Bevilacqua <nicobevilacqua@gmail.com>
2022-10-03 18:34:26 -0300

    fix ethernaut-dao-token test

--- test/05-EthernautDAOToken.test.ts
+++ test/05-EthernautDAOToken.test.ts
@@ -3,16 +3,30 @@ import { expect } from 'chai';
 import { Contract } from 'ethers';
 import { ethers } from 'hardhat';
 
+const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+
+const addressWithBalance = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266';
+
 describe('EthernautDaoToken', () => {
   let target: Contract;
   let attacker: SignerWithAddress;
   let deployer: SignerWithAddress;
   before(async () => {
-    [attacker, deployer] = await ethers.getSigners();
+    [, attacker, deployer] = await ethers.getSigners();
 
     target = await (await ethers.getContractFactory('EthernautDaoToken', deployer)).deploy();
 
     await target.deployed();
+
+    await (
+      await target.mint(addressWithBalance, ethers.utils.parseEther('0.000000000000000001'))
+    ).wait();
+    await (
+      await target.mint(addressWithBalance, ethers.utils.parseEther('0.099999999999999999'))
+    ).wait();
+    await (
+      await target.mint(addressWithBalance, ethers.utils.parseEther('0.999999999999999999'))
+    ).wait();
 
     target = target.connect(attacker);
   });
@@ -22,6 +36,6 @@ describe('EthernautDaoToken', () => {
      * YOUR CODE HERE
      */
 
-    expect((await target.balanceOf(attacker.address)).eq(await target.totalSupply())).to.be.true;
+    expect(ethers.utils.formatEther(await target.balanceOf(addressWithBalance))).to.equal('0.0');
   });
 });
